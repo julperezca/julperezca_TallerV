584a8eae4b200b8c6f321a5f423745be7232d5d0
Julian <julperezca@unal.edu.co>
2025-02-20 19:46:06 -0500

    adicion de registros a configurar en rtc driver

--- PeripheralsDrivers/Src/rtc_driver_hal.c
+++ PeripheralsDrivers/Src/rtc_driver_hal.c
@@ -6,14 +6,58 @@
 
 
 void algo(void){
-	RCC->BDCR |= RCC_BDCR_LSEON; // crear un delay para tener el LSE activo
+
+	RCC->BDCR |= RCC_BDCR_LSEON;
+
+	// crear un delay para tener el LSE activo
+	if (!(RCC->BDCR & RCC_BDCR_LSERDY)) {
+	        RCC->BDCR |= RCC_BDCR_LSEON;
+	        // registro que alza la bandera para saber si está listo el LSE
+	        while (!(RCC->BDCR & RCC_BDCR_LSERDY));
+	 }
+
 	 // habilitar el clock del RTC
 
-	RCC_BDCR_LSERDY; // registro que alza la bandera para saber si está bien o no el LSE
 	// crear una función  para la elección del reloj: por ahora será LSE pedida en la tarea // escoger reloj NoClock, LSE, LSI, HSE
-	RCC->BDCR  = RCC_BDCR_RTCEN;
+
+	RCC->BDCR &= ~RCC_BDCR_RTCSEL_0; // limpia registro
 	RCC->BDCR |= RCC_BDCR_RTCSEL_0;  // 01 corresponde a LSE clock
+
+	 // habilitar el clock del RTC
+	RCC->BDCR  = RCC_BDCR_RTCEN;
+
+	// pg 438 registro de protección de escritura del RTC-> se desactiva
+	// dice que se debe escribir :
+
+	//	Write ‘0xCA’ into the RTC_WPR register.
+	// Write ‘0x53’ into the RTC_WPR register. Si se aplica uno diferente se activa la protección de escritura
+
+	RTC->WPR = 0xCA;
+	RTC->WPR = 0x53;
+
+	// está el registro de RTC initialization and statud register RTC_ISR
+
+	// se inicializa
+	RTC->ISR |= RTC_ISR_INIT;
+	while (!(RTC->ISR & RTC_ISR_INITF));
+
+
 
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
